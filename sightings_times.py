a677e38d8cc81367d2bc56fdaa56b59180e5c691
wslider <wslider2000@gmail.com>
2026-05-13 16:49:54 -0400

    moved csv files into data/ folder + sightings and weather conditions content in complete_analysis.ipynb

--- sightings_times.py
+++ sightings_times.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv("complete.csv", usecols=range(0, 11))
+df = pd.read_csv("data/complete.csv", usecols=range(0, 11))
 df.info()
 
 df['datetime'] = pd.to_datetime(df['datetime'].astype(str), errors='coerce')
